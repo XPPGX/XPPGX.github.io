ab35ff7fb1f5eebc3aaf0fb97fc167d0c70ebf1a
Arthur <qwerlegendary@gmail.com>
2020-11-13 20:40:36 +0800

    Initial commit

---
+++ main_test.js
@@ -0,0 +1,27 @@
+if (Hls.isSupported()) {
+    const video1 = document.getElementById("video1");
+    const hls1 = new Hls();
+    hls1.attachMedia(video1);
+  
+    hls1.on(Hls.Events.MEDIA_ATTACHED, function() {
+      console.log("video and hls.js are now bound together !");
+      hls1.loadSource(
+        "http://192.168.1.114:8080/hls/test.m3u8"
+      );
+    });
+  
+    const video2 = document.getElementById("video2");
+    const hls2 = new Hls();
+    hls2.attachMedia(video2);
+  
+    hls2.on(Hls.Events.MEDIA_ATTACHED, function() {
+      console.log("video and hls.js are now bound together !");
+      hls2.loadSource(
+        "http://192.168.1.114:8080/hls/ayame.m3u8"
+      );
+    });
+    
+
+
+
+  }
